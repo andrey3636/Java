a249ce283046e34aab27d85f6894b77929ad0e23
Andrey3636 <bmw19860901@gmail.com>
2022-10-16 08:59:54 +0200

    first commit

---
+++ Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        int intnum1 = 54;
+        int intnum2 =16;
+        int sum1 = intnum1 + intnum2;
+        System.out.println(sum1);
+//----------------------------------/
+        int intnum3 = 50;
+        int intnum4 = 3;
+        int sum2 = intnum3 / intnum4;
+        System.out.println(sum2);
+
+        int intvalue1 = 2;
+        int intvalue2 = 2;
+        int sum = intvalue1 * intvalue2;
+        System.out.println(sum);
+    }
+}
